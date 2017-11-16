8d735ec81a36315eb9e99e574b4ccc7b2fdf5ea5
Fielding Johnston <fielding@justfielding.com>
2017-11-16 09:51:02 -0600

    Fix typos in tests

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 const expect  = require('chai').expect;
 const fetch = require('node-fetch');
 
-const url = 'http://localhost:3030';
+const url = 'http://localhost:3040';
 
 const server = require('../index');
 
@@ -13,7 +13,7 @@ describe('General functionality', () => {
 });
 
 describe('Upload action', () => {
-  it('service endpoint is reachable', async () => {
+  it('upload endpoint is reachable', async () => {
     const response = await fetch(`${url}/upload`);
     expect(response.status).to.equal(200);
   });
